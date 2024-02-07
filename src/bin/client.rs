3f78d8e41b3fa29e1bfa6040634274ab48eb4038
Tirth Patel <tirth1093@gmail.com>
2024-02-07 20:43:08 +0530

    use tokio, different threads for sending and receiving [currently poor performance due to slow receive thread

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,10 +1,16 @@
 use std::fs::File;
 use std::io::{Read, Write};
 use std::net::TcpStream;
+use std::thread;
 use std::time::SystemTime;
 use std::time::{Duration, Instant};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
-fn main() {
+use tokio::time::sleep;
+use tokio::try_join;
+
+#[tokio::main]
+async fn main() {
     let args = std::env::args().collect::<Vec<String>>();
     if args.len() != 3 {
         eprintln!("Usage: client <ip> <duration_in_seconds>");
@@ -17,10 +23,10 @@ fn main() {
     println!("Connecting to server at {}", ip);
     println!("Sending latency packets for {} seconds", duration);
 
-    client(ip, Duration::from_secs(duration));
+    client(ip, Duration::from_secs(duration)).await;
 }
 
-fn client(ip: &String, test_duration: Duration) {
+async fn client(ip: &String, test_duration: Duration) {
     let start_time = Instant::now();
     let end_time = start_time + test_duration;
 
@@ -29,57 +35,79 @@ fn client(ip: &String, test_duration: Duration) {
         File::create("output/latency_from_payload.txt").expect("Failed to create file_payload");
 
     // connect to the server at the specified IP address and 1155 port
-    let mut stream =
-        TcpStream::connect(format!("{}:1155", ip)).expect("Failed to connect to server");
+    // let mut stream =
+    //     TcpStream::connect(format!("{}:1155", ip)).expect("Failed to connect to server");
+    let mut stream = tokio::net::TcpStream::connect(format!("{}:1155", ip))
+        .await
+        .expect("Failed to connect to server");
+
+    let (mut reader, mut writer) = stream.into_split();
 
     let mut send_count: u64 = 0;
     let mut recv_count: u64 = 0;
 
-    // sending latency packets for the specified duration
-    while Instant::now() < end_time {
-        let timestamp = SystemTime::now()
-            .duration_since(SystemTime::UNIX_EPOCH)
-            .expect("Failed to get timestamp")
-            .as_nanos();
-        // println!("{:?}", timestamp);
+    let send_handle = tokio::spawn(async move {
+        while Instant::now() < end_time {
+            // println!("Sending packet to server");
+            let timestamp = SystemTime::now()
+                .duration_since(SystemTime::UNIX_EPOCH)
+                .expect("Failed to get timestamp")
+                .as_nanos();
+            // println!("{:?}", timestamp);
 
-        let packet = &timestamp.to_be_bytes();
-        stream
-            .write_all(packet)
-            .expect("Failed to send packet to server");
-        send_count += 1;
+            let packet = &timestamp.to_be_bytes();
+            writer
+                .write_all(packet)
+                .await
+                .expect("Failed to send packet to server");
+            send_count += 1;
+        }
+        println!("Sent {} packets", send_count);
+    });
 
-        // thread::sleep(Duration::from_secs(1)); // simulate network delay
-
-        // receive packet from server
+    let recv_handle = tokio::spawn(async move {
         let mut buffer = [0; 16];
-        let bytes_read = stream
-            .read(&mut buffer)
-            .expect("Failed to read from server");
-        // println!("Received {} bytes", bytes_read);
-        recv_count += 1;
+        // while no response from channel, keep receiving packets
+        loop {
+            // println!("\tReceiving packet from server");
+            // receive packet from server
+            let bytes_read = reader
+                .read(&mut buffer)
+                .await
+                .expect("Failed to read from server");
+            // println!("Received {} bytes", bytes_read);
+            recv_count += 1;
 
-        let current_timestamp = SystemTime::now()
-            .duration_since(SystemTime::UNIX_EPOCH)
-            .expect("Failed to get timestamp")
-            .as_nanos();
+            let current_timestamp = SystemTime::now()
+                .duration_since(SystemTime::UNIX_EPOCH)
+                .expect("Failed to get timestamp")
+                .as_nanos();
 
-        // process the received packet
-        // let received_packet_str = String::from_utf8_lossy(received_packet);
-        let received_timestamp = u128::from_be_bytes(buffer[..bytes_read].try_into().unwrap());
-        // println!("\t{}", received_timestamp);
+            // process the received packet
+            // let received_packet_str = String::from_utf8_lossy(received_packet);
+            let be_bytes = buffer[..bytes_read].try_into();
+            let be_bytes = match be_bytes {
+                Ok(bytes) => bytes,
+                Err(_) => {
+                    eprintln!("Failed to convert to bytes");
+                    break;
+                }
+            };
+            let received_timestamp = u128::from_be_bytes(be_bytes);
+            // println!("\t{}", received_timestamp);
 
-        let latency_from_payload = current_timestamp - received_timestamp;
-        // println!("Latency (from payload): {} ns", latency_from_payload);
+            let latency_from_payload = current_timestamp - received_timestamp;
+            // println!("Latency (from payload): {} ns");
 
-        // write latency values to file
-        writeln!(file_payload, "{}", latency_from_payload)
-            .expect("Failed to write to file_payload");
-    }
+            // write latency values to file
+            writeln!(file_payload, "{}", latency_from_payload)
+                .expect("Failed to write to file_payload");
+        }
+        println!("Received {} packets", recv_count);
+        file_payload.flush().expect("Failed to flush file_payload");
+    });
 
-    println!("Sent {} packets", send_count);
-    println!("Received {} packets", recv_count);
+    try_join!(send_handle, recv_handle).expect("Failed to join threads");
 
     // close the file latency values file
-    file_payload.flush().expect("Failed to flush file_payload");
 }
